a806bd630aeb3bea7a1d21ccc899b9a61f663540
Ahmed Teleb <ahmedasd37@gmail.com>
2019-07-15 15:57:43 +0200

    add _ finish task

--- app/src/main/java/com/ahmedteleb/andelachallange1/AboutActivity.java
+++ app/src/main/java/com/ahmedteleb/andelachallange1/AboutActivity.java
@@ -1,13 +1,33 @@
 package com.ahmedteleb.andelachallange1;
 
+import android.net.http.SslError;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class AboutActivity extends AppCompatActivity {
 
+    WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);   //show back button
+
+        webView =  findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+        });
+        webView.loadUrl("https://andela.com/alc");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
     }
 }
